0c676e1256d736b6958da33fa24bf025c7b87942
Carrie8348 <maruko_carrie@hotmail.com>
2022-03-17 07:52:07 +0000

    add fetchGitHubInformation function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,7 +1,7 @@
 function fetchGitHubInformation(event) {
     var username = $("#gh-username").val();
     if(!username) {
-        ${"#gh-user-data"}.html(`<h2>please enter a GitHub username</h2>`);
+        $("#gh-user-data").html(`<h2>please enter a GitHub username</h2>`);
         return;
     }
 
